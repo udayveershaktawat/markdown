68a2f7cf4a5602b3c53fe541769b6d8b221fa81b
udayveershaktawat <udayveershaktawat@gmail.com>
2024-07-05 19:41:13 +0530

    kkkkk

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import { useEffect } from 'react';
 import './App.css';
 
 import CustomMarkDown from './components/CustomMarkDown';
@@ -6,6 +6,24 @@ import CustomMarkDown from './components/CustomMarkDown';
 
 
 function App() {
+  useEffect(() => {
+    const sendHeight = () => {
+      const height = document.documentElement.scrollHeight;
+      window.parent.postMessage({ height }, '*');
+    };
+
+    // Send height on load
+    sendHeight();
+
+    // Send height on resize
+    window.addEventListener('resize', sendHeight);
+
+    // Clean up event listener
+    return () => {
+      window.removeEventListener('resize', sendHeight);
+    };
+  }, []);
+
   return (<>
     
     <CustomMarkDown></CustomMarkDown>
